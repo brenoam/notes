ad80897465bcfd81c7e79bd8c03972e482ceda36
brenoam <brenoam@dcc.ufmg.br>
2020-03-21 00:11:28 -0300

    Color picker

--- src/scripts/Note.js
+++ src/scripts/Note.js
@@ -1,13 +1,14 @@
 export default class Note {
-	constructor({id, title, text}, notesController) {
+	constructor({id, title, text, color}, notesController) {
 		this.id = id;
 		this.title = title;
 		this.text = text;
 		this.notesController = notesController;
+		this.color = color || "#f5d442";
 	}
 	
 	getTemplate() {
-		return `<div class="note flex-item" id="{{id}}">
+		return `<div class="note flex-item" id="{{id}}" style="--note-color:{{color}}">
 			<div class="note-delete">
 				<i class="fas fa-times"></i>
 			</div>
@@ -22,7 +23,7 @@ export default class Note {
 
 	getEditTemplate() {
 		return `<div class="modal">
-			<div class="modal-note-edit">
+			<div class="modal-note-edit" style="--note-color:{{color}}">
 				<div class="note-delete">
 					<i class="fas fa-times"></i>
 				</div>
@@ -31,6 +32,13 @@ export default class Note {
 				</div>
 				<div class="note-text" contenteditable="true">
 					{{text}}
+				</div>
+				<div class="color-picker flex-container">
+					<span class="color-item" style="--pckr-color:#F5D442"> </span>
+					<span class="color-item" style="--pckr-color:#F08080"> </span>
+					<span class="color-item" style="--pckr-color:#87CEFA"> </span>
+					<span class="color-item" style="--pckr-color:#40E0D0"> </span>
+					<span class="color-item" style="--pckr-color:#8FBC8F"> </span>
 				</div>
 			</div>
 		</div>`;
@@ -41,6 +49,7 @@ export default class Note {
 		el = el.replace('{{id}}', this.id);
 		el = el.replace('{{title}}', this.title?this.title: "");
 		el = el.replace('{{text}}', this.text? this.text: "");
+		el = el.replace('{{color}}', this.color);
 		return el;
 	}
 
@@ -48,6 +57,7 @@ export default class Note {
 		let el = this.getEditTemplate();
 		el = el.replace('{{title}}', this.title?this.title: "");
 		el = el.replace('{{text}}', this.text? this.text: "");
+		el = el.replace('{{color}}', this.color);
 		return el;
 	}
 
@@ -79,8 +89,10 @@ export default class Note {
 		let modalContent = document.getElementsByClassName("modal-note-edit")[0];
 		let title = modalContent.getElementsByClassName("note-title")[0].innerText;
 		let text = modalContent.getElementsByClassName("note-text")[0].innerText;
+		let color = modalContent.style.getPropertyValue("--note-color");
 		this.title = title;
 		this.text = text;
+		this.color = color;
 		let oldNote = document.getElementById(this.id);
 
 		let noteHtml = this.getRenderEl();
@@ -103,10 +115,19 @@ export default class Note {
 		note.addEventListener('click', this.renderNoteEdit.bind(this), false);
 	}
 
+	changeModalColor() {
+		let color = this.style.getPropertyValue("--pckr-color");
+		document.getElementsByClassName("modal-note-edit")[0].style.setProperty("--note-color", color);
+	}
+
 	addModalEventListeners() {
 		let modal = document.getElementsByClassName("modal")[0]
 		let modalContent = modal.getElementsByClassName("modal-note-edit")[0]
 		modalContent.getElementsByClassName("note-delete")[0].addEventListener('click', this.removeModal);
+		let colorPicker = modalContent.getElementsByClassName("color-item");
+		Object.keys(colorPicker).forEach(key => {
+			colorPicker[key].addEventListener('click', this.changeModalColor);
+		});
 		modalContent.addEventListener("click", (e) => e.stopPropagation(), false);
 		modal.addEventListener('click', this.saveNote.bind(this), false);
 	}
